2cb7540c22ced355dbc514706904f64f59b89a7c
Rujun Yao <pandalv9999@gmail.com>
2020-11-05 08:58:23 -0800

    Code review from Rujun Yao

--- back/app.js
+++ back/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
+// there is not folder named "front" in the back folder. "../front/build" will be a proper way
 
 app.use("/", indexRouter);
 
